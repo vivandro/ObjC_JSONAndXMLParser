833bc7ed4315ce9211f840833e035d3b9ec47453
Vichare, Vivek <vivekvi@amazon.com>
2015-03-08 13:32:57 -0700

    Initial Commit

---
+++ vTutorial_ParsingJSONandXML/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vTutorial_ParsingJSONandXML
+//
+//  Created by Vichare, Vivek on 3/8/15.
+//  Copyright (c) 2015 Vichare, Pallavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
